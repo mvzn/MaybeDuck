c78eb4381356c01b0a01635b9f5643ecb996e0a0
Mark <mark.movzon@gmail.com>
2026-03-24 20:46:39 +0000

    JUCE addition

---
+++ MaybeDuck/Builds/LinuxMakefile/build/intermediate/Release/include_juce_audio_processors_headless_ara_8deeb88d.d
@@ -0,0 +1,8 @@
+build/intermediate/Release/include_juce_audio_processors_headless_ara_8deeb88d.o: \
+ ../../JuceLibraryCode/include_juce_audio_processors_headless_ara.cpp \
+ ../../../../JUCE/modules/juce_audio_processors_headless/juce_audio_processors_headless_ara.cpp \
+ ../../../../JUCE/modules/juce_core/system/juce_CompilerWarnings.h \
+ ../../../../JUCE/modules/juce_core/system/juce_TargetPlatform.h \
+ ../../../../JUCE/modules/juce_audio_processors_headless/format/juce_PluginFormatDefs.h \
+ ../../../../JUCE/modules/juce_audio_processors_headless/utilities/ARA/juce_ARADebug.h \
+ ../../../../JUCE/modules/juce_core/system/juce_PlatformDefs.h
---
+++ MaybeDuck/Builds/LinuxMakefile/build/intermediate/Release/include_juce_audio_processors_headless_lv2_libs_36180e32.d
@@ -0,0 +1,6 @@
+build/intermediate/Release/include_juce_audio_processors_headless_lv2_libs_36180e32.o: \
+ ../../JuceLibraryCode/include_juce_audio_processors_headless_lv2_libs.cpp \
+ ../../../../JUCE/modules/juce_audio_processors_headless/juce_audio_processors_headless_lv2_libs.cpp \
+ ../../../../JUCE/modules/juce_core/system/juce_CompilerWarnings.h \
+ ../../../../JUCE/modules/juce_core/system/juce_TargetPlatform.h \
+ ../../../../JUCE/modules/juce_audio_processors_headless/format/juce_PluginFormatDefs.h
