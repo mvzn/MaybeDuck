a0d8c8d8378bec2d0c7ad2f3f5b8db8a84312ded
Mark <mark.movzon@gmail.com>
2026-03-24 20:45:30 +0000

    JUCE addition

--- denormals.cpp
+++
@@ -1,8 +0,0 @@
-#include <xmmintrin.h>
-#include <pmmintrin.h>
-
-inline void enableFTZ_DAZ()
-{
-    _MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
-    _MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
-}
